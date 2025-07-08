ec0eac94c283da7a45f49828865da438baf27936
ncm <github@ncmcodes.com>
2025-07-08 14:53:50 -0400

    Ditto

--- backend/quiz/manage.py
+++ backend/quiz/manage.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz.settings")
+    if os.environ.get("DJANGO_DEBUG", "").lower() == "true":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz.settings.development")
+    elif os.environ.get("DJANGO_DEBUG", "").lower() == "tests":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz.settings.tests")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
