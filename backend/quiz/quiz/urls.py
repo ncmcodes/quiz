7f28c5ac30ad3948c5d959a96c7ac05e8e5656bf
ncm <github@ncmcodes.com>
2025-07-27 16:28:58 -0400

    Add favicon

--- backend/quiz/quiz/urls.py
+++ backend/quiz/quiz/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from quizAPI.views import index, health_check, auth_status
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path("", index),
@@ -11,4 +13,8 @@ urlpatterns = [
     path("auth/", include("quizAUTH.urls")),
     path("auth/status/", auth_status),
     path("health/", health_check),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon/favicon.ico")),
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
